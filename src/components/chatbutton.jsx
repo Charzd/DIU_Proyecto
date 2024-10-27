f8a003f4758b84d40b6e8f833f77286c280e9d5d
Carlos Saavedra <carlos.saavedrad@sansano.usm.cl>
2024-10-27 16:25:52 -0400

    cambios chatbot

--- src/components/chatbutton.jsx
+++ src/components/chatbutton.jsx
@@ -1,20 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { BsChatDots } from 'react-icons/bs'; 
 import Chatbot from './chatbot'; 
 
 const ChatButton = () => {
   const [isChatOpen, setIsChatOpen] = useState(false);
+  const chatbotRef = useRef(null);
 
   const toggleChat = () => {
     setIsChatOpen(!isChatOpen);
   };
+
+  // Función para manejar clics fuera del chatbot
+  const handleClickOutside = (event) => {
+    if (chatbotRef.current && !chatbotRef.current.contains(event.target)) {
+      setIsChatOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    // Agregar event listener al montar el componente
+    document.addEventListener('mousedown', handleClickOutside);
+    
+    // Limpiar el event listener al desmontar el componente
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
 
   return (
     <div>
       <button className="chat-button" onClick={toggleChat}>
         <BsChatDots size={24} color="#000" />
       </button>
-      {isChatOpen && <Chatbot />}
+      {isChatOpen && (
+        <div ref={chatbotRef}>
+          <Chatbot />
+        </div>
+      )}
     </div>
   );
 };
