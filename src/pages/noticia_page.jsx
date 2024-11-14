406e5b598c0ca452a7c2f38fab032d32a8ac4eae
Carlos Saavedra <carlos.saavedrad@sansano.usm.cl>
2024-11-13 21:22:53 -0400

    volver a resultados

--- src/pages/noticia_page.jsx
+++ src/pages/noticia_page.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../stylesheets/noticia-page/noticia-page.scss';
 import noticia from '../assets/noticia.png';
 
 const NoticiaPage = () => {
+  const navigate = useNavigate();
+
+  const handleBackClick = () => {
+    navigate(-1);
+  };
+
   return (
     <div className="noticia">
-      <h1 className="noticia__titulo">Título de Noticia 3</h1>
-      <img src={noticia} alt="Noticia 3" className="noticia__imagen" />
-      <p className="noticia__contenido">
-        Descripción Noticia 3
-      </p>
+      <div className="noticia__contenido-enmarcado">
+        <h1 className="noticia__titulo">Título de Noticia 3</h1>
+        <img src={noticia} alt="Noticia 3" className="noticia__imagen" />
+        <p className="noticia__contenido">
+          Descripción Noticia 3
+        </p>
+      </div>
+      <button onClick={handleBackClick} className="noticia__volver">
+        Volver a Resultados
+      </button>
     </div>
   );
 };
